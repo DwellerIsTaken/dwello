a8c467fdb678098ab19a01e98ef478ca6e6b6326
DwellerIsTaken <wobbler555@outlook.com>
2023-03-30 16:08:58 +0200

    cogs from Cog

--- cogs/entertainment/__init__.py
+++ cogs/entertainment/__init__.py
@@ -4,7 +4,8 @@ from .bot_eco import Bot_Economy
 from .guild_eco import Guild_Economy
 
 class Entertainment(Bot_Economy, Guild_Economy):
-    pass
+    def __init__(self, bot):
+        super().__init__(bot=bot)
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(Entertainment(bot))
