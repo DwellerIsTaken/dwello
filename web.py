3e747622647e1605e490dd3d62757c81e71b1d59
DwellerIsTaken <wobbler555@outlook.com>
2023-04-18 15:17:04 +0200

    web

---
+++ web.py
@@ -0,0 +1,10 @@
+from aiohttp import web
+
+async def handle_post(request):
+    print("It works!")
+    return web.Response(text="Hello from aiohttp!")
+
+app = web.Application()
+app.router.add_post('/aiohttp', handle_post)  # Add a route for handling post requests
+
+web.run_app(app, host='0.0.0.0', port=8080)
