36569e574687aa79fafd3d78e414021c7f1bb07a
ItsWilliboy <83978878+itswilliboy@users.noreply.github.com>
2023-07-14 16:27:38 +0200

    todo update

--- cogs/other/todo.py
+++ cogs/other/todo.py
@@ -2,11 +2,11 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 from datetime import datetime
-from typing import TYPE_CHECKING, Optional, Tuple
+from typing import TYPE_CHECKING, Optional, Tuple, List, Union
 
 import dateparser
 import discord
-from discord import Interaction, app_commands
+from discord import Interaction as DiscordInteraction, app_commands
 from discord.ext import commands
 from discord.ui import Button, Modal, TextInput, View
 from typing_extensions import Self
@@ -18,6 +18,8 @@ if TYPE_CHECKING:
 
     from core import Dwello, DwelloContext
 
+    Interaction = DiscordInteraction[Dwello]
+
 
 class EditDueDateButton(Button):
     def __init__(self, todo: TodoItem, cog: Todo, *, label: str = "Add Due Date"):
@@ -27,7 +29,7 @@ class EditDueDateButton(Button):
         self.label: str = label
         self.style: discord.ButtonStyle = discord.ButtonStyle.green
 
-    async def callback(self, interaction: Interaction[Dwello]):
+    async def callback(self, interaction: Interaction):
         if interaction.user.id != self.todo.user_id:
             return await interaction.response.send_message("This button is not for you.", ephemeral=True)
 
@@ -49,7 +51,7 @@ class EditDueDateModal(Modal, title="Edit Due Date"):
         self.todo: TodoItem = todo
         self.cog: Todo = cog
 
-    async def on_submit(self, interaction: Interaction[Dwello]):
+    async def on_submit(self, interaction: Interaction):
         date = dateparser.parse(self.due_date.value, settings={"PREFER_DATES_FROM": "future"})
         if date is None:
             return await interaction.response.send_message(
@@ -93,7 +95,7 @@ class TodoAddModal(Modal, title="Add A Todo!"):
         self.cog: Todo = cog
         self.message: discord.Message = message
 
-    async def on_submit(self, interaction: Interaction[Dwello]) -> None:
+    async def on_submit(self, interaction: Interaction) -> None:
         date: Optional[datetime] = None
         if value := self.due_date.value:
             date = dateparser.parse(value, settings={"PREFER_DATES_FROM": "future"})
@@ -139,6 +141,120 @@ class TodoItem:
             record["completed_at"],
         )
 
+UserMember = Union[discord.User, discord.Member]
+
+class TodoPaginator(View):
+    def __init__(self, cog: Todo, todos: List[TodoItem], author: UserMember) -> None:
+        super().__init__()
+        self.cog: Todo = cog
+        self.todos: List[TodoItem] = todos
+        self.message: discord.Message
+        self.author: UserMember = author
+
+        self._current_page: int = 0
+
+    @property
+    def length(self) -> int:
+        return len(self.todos)
+
+    @property
+    def current_page(self) -> int:
+        return self._current_page
+
+    @property
+    def index(self) -> str:
+        return f"Todo {self.current_page + 1}/{self.length}"
+    
+    async def interaction_check(self, interaction: Interaction) -> bool:
+        if interaction.user != self.author:
+            await interaction.response.send_message("This button is not for you", ephemeral=True)
+            return False
+        return True
+
+    async def get_page(self, *, page: Optional[int] = None, next: Optional[bool] = None) -> Optional[discord.Embed]:
+        if page is None and next is None or all((page, next)):
+            raise ValueError("change_page: Only one of two values must be supplied.")
+
+        next_or_previous: int = 0
+        if page is None:
+            if next is True:
+                next_or_previous = 1
+            else:
+                next_or_previous = -1
+
+        _page = page if page is not None else self.current_page + next_or_previous
+
+        if _page < 0 or _page > self.length:
+            return None
+
+        self._current_page = _page
+
+        self._next.disabled = False
+        self._previous.disabled = False
+
+        if _page + 1 == self.length:
+            self._next.disabled = True
+
+        elif _page == 0:
+            self._previous.disabled = True
+
+        self.add_message_jump_button()
+        embed = await self.cog.get_embed(self.todos[_page])
+        return embed[0]
+
+    def add_message_jump_button(self) -> None:
+        todo = self.todos[self.current_page]
+        if todo.message_id:
+            jump_url = f"https://discord.com/channels/{todo.guild_id}/{todo.channel_id}/{todo.message_id}"
+            button = Button(label="Jump To Message", url=jump_url, row=0)
+            return self._children.insert(2, button)
+        
+        if self.children[2].label == "Jump To Message":
+            del self._children[2]
+
+    @discord.ui.button(label="Previous", row=0, style=discord.ButtonStyle.green)
+    async def _previous(self, interaction: Interaction, _):
+        embed = await self.get_page(next=False)
+        await interaction.response.edit_message(content=self.index, embed=embed, view=self)
+
+    @discord.ui.button(label="Next", row=0, style=discord.ButtonStyle.green)
+    async def _next(self, interaction: Interaction, _):
+        embed = await self.get_page(next=True)
+        await interaction.response.edit_message(content=self.index, embed=embed, view=self)
+
+    @discord.ui.button(label="Go To Page", row=1, style=discord.ButtonStyle.green)
+    async def _go_to_page(self, interaction: Interaction, _):
+        await interaction.response.send_modal(GoToPageModal(self))
+
+    @discord.ui.button(label="Go To Todo With ID", row=1, style=discord.ButtonStyle.green, disabled=True)
+    async def _go_to_id(self, interaction: Interaction, _):
+        await interaction.response.send_message("Not implemented :3", ephemeral=True)
+
+
+class GoToPageModal(Modal, title="Go To Page"):
+    def __init__(self, paginator: TodoPaginator) -> None:
+        super().__init__()
+        self.paginator: TodoPaginator = paginator
+
+        self.page: TextInput = TextInput(label="Page", min_length=1, max_length=3, placeholder=f"1-{self.paginator.length}")
+        self.add_item(self.page)
+
+    async def on_submit(self, interaction: Interaction) -> None:
+        async def error() -> None:
+            msg = f"Couldn't find that todo, make sure it's a number between `1` and `{self.paginator.length}`"
+            await interaction.response.send_message(msg, ephemeral=True)
+
+        try:
+            page = int(self.page.value)
+        except ValueError:
+            return await error()
+
+        if page <= 0 or page > self.paginator.length:
+            return await error()
+
+        embed = await self.paginator.get_page(page=page - 1)
+        await interaction.response.edit_message(content=self.paginator.index, embed=embed, view=self.paginator)
+
 
 class Todo(BaseCog):
     def __init__(self, bot: Dwello) -> None:
@@ -178,18 +294,32 @@ class Todo(BaseCog):
         todo.id = record["id"]
         return todo
 
-    async def get_todo(self, id: int) -> TodoItem:
+    async def get_todo(self, id: int) -> Optional[TodoItem]:
         """Gets a todo item from the database"""
         query = """
             SELECT * FROM todo WHERE id = $1
         """
         async with self.bot.pool.acquire() as conn:
             result = await conn.fetchrow(query, id)
-            if result is None:
-                return
 
-            todo = TodoItem.from_record(result)
-            return todo
+        if result is None:
+            return
+
+        todo = TodoItem.from_record(result)
+        return todo
+
+    async def get_todos(self, user_id: int) -> List[Optional[TodoItem]]:
+        """Gets all of a users todos"""
+        query = """
+            SELECT * FROM todo WHERE user_id = $1
+        """
+        async with self.bot.pool.acquire() as conn:
+            result = await conn.fetch(query, user_id)
+
+        if not result:
+            return []
+
+        return [TodoItem.from_record(i) for i in result]
 
     async def get_embed(self, todo: TodoItem) -> Tuple[discord.Embed, Optional[View]]:
         embed = discord.Embed(title=f"Todo #{todo.id}", color=0x2B2D31)
@@ -226,7 +356,7 @@ class Todo(BaseCog):
 
         return embed, view
 
-    async def context_menu(self, interaction: Interaction[Dwello], message: discord.Message):
+    async def context_menu(self, interaction: Interaction, message: discord.Message):
         modal = TodoAddModal(self, message)
         await interaction.response.send_modal(modal)
 
@@ -258,3 +388,13 @@ class Todo(BaseCog):
 
         embed, view = await self.get_embed(todo)
         await ctx.send(embed=embed, view=view)
+
+    @todo.command()
+    async def list(self, ctx: DwelloContext):
+        todos = await self.get_todos(ctx.author.id)
+        if not todos:
+            return await ctx.send("You don't have any todos.")
+
+        view = TodoPaginator(self, todos, ctx.author)
+        first_page = await view.get_page(page=0)
+        view.message = await ctx.send(view.index, embed=first_page, view=view)
